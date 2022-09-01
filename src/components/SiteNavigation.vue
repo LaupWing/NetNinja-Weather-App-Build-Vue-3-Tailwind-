15477ac1b837d474207baf70ed1fd7d6cbe78a32
LaupWing <loc_strict@hotmail.com>
2022-09-01 14:39:08 +0200

    This all for hte funcking sun

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -2,7 +2,7 @@
    <header class="sticky top-0 bg-weather-primary shadow-lg">
       <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py6">
          <div class="flex items-center gap-3 flex-1">
-            
+            <font-awesome-icon icon="fa-solid fa-sun" />
          </div>
       </nav>
    </header>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faSun} from '@fortawesome/free-solid-svg-icons'
 
+library.add(faSun)
 
 import './assets/index.css'
 
 const app = createApp(App)
-   .component("font-awesome-ion", FontAwesomeIcon)
+   .component("font-awesome-icon", FontAwesomeIcon)
 
 app.use(router)
 
