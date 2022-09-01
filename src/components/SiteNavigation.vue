d737282a622ca50287e8f829fa784bf85d1ce488
LaupWing <loc_strict@hotmail.com>
2022-09-01 14:33:05 +0200

    added custom font awesome icon

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,7 +1,11 @@
 <template>
-   <div>
-      
-   </div>
+   <header class="sticky top-0 bg-weather-primary shadow-lg">
+      <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py6">
+         <div class="flex items-center gap-3 flex-1">
+            
+         </div>
+      </nav>
+   </header>
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 import './assets/index.css'
 
 const app = createApp(App)
+   .component("font-awesome-ion", FontAwesomeIcon)
 
 app.use(router)
 
