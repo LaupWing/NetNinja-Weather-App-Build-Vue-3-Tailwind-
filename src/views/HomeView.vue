203ac5fa77f7e011cb6e9bd45863228384c43c65
LaupWing <loc_strict@hotmail.com>
2022-09-02 19:37:13 +0200

    erros

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,18 @@
             class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
          >
       </div>
+      <ul 
+         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+         v-if="mapboxSearchResults"
+      >
+         <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+         >
+            {{searchResult.place_name}}
+         </li>
+      </ul>
    </main>
 </template>
 
@@ -18,12 +30,22 @@ import axios from 'axios'
 const mapBoxAPIKey = ""
 const searchQuery = ref("")
 const queryTimeout = ref(null)
+const mapboxSearchResults = ref(null)
+const searchError = ref(null)
 
 const getSearchResults = () =>{
+   clearTimeout(queryTimeout.value)
    queryTimeout.value = setTimeout(async ()=>{
       if(searchQuery.value !== ""){
-         const result = await axios.get(``)
+         try{
+            const result = await axios.get(``)
+            mapboxSearchResults.value = result.data.features
+         }catch {
+            searchError.value = true
+         }
+         return
       }
+      mapboxSearchResults.value = null
    },300)
 }
 
