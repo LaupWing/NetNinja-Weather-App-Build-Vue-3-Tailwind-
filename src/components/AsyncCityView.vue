073b4d39a6017ba255ad57e4be78d0c97cbebb8c
LaupWing <loc_strict@hotmail.com>
2022-09-05 13:46:12 +0200

    9

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,6 +1,86 @@
 <template>
-   <div>
-      
+   <div class="flex flex-col flex-1 items-center">
+      <div
+         v-if="route.query.preview"
+         class="text-white p-4 bg-weather-secondary w-full text-center"
+      >
+         <p>You are currently previewing this city, click on the "+" icon to start tracking this city</p>
+      </div>
+      <div class="flex flex-col items-center text-white py-12">
+         <h1 class="text-4xl mb-2">
+            {{route.params.city}}
+         </h1>
+         <p class="text-sm mb-12">
+            {{
+               new Date(weatherData.currentTime).toLocaleDateString(
+                  "en-us",
+                  {
+                     weekday: "short",
+                     day: "2-digit",
+                     month: "long"
+                  }
+               )
+            }}
+            {{
+               new Date(weatherData.currentTime).toLocaleDateString(
+                  "en-us",
+                  {
+                     timeStyle: "short"
+                  }
+               )
+            }}
+         </p>
+         <p class="text-8xl mb-8">
+            {{Math.round(weatherData.current.temp)}}&deg;
+         </p>
+         <p>
+            Feels like
+            {{Math.round(weatherData.current.feels_like)}} &deg;
+         </p>
+         <p class="capitalize">
+            {{weatherData.current.weather[0].description}}
+         </p>
+         <img 
+            class="w-[150px] h-auto"
+            :src="``" 
+            alt=""
+         >
+      </div>
+      <hr class="border-white border-opacity-10 w-full"/>
+      <div class="max-w-screen-md w-full py-12">
+         <div class="mx-8 text-white">
+            <h2 class="mb-4">
+               Hourly weather
+            </h2>
+            <div class="flex gap-10 overflow-x-scroll">
+               <div
+                  v-for="hourData in weatherData.hourly"
+                  :key="hourData.dt"
+                  class="flex flex-col gap-4 items-center"
+               >
+                  <p class="whitespace-nowrap text-md">
+                     {{
+                        new Date(
+                           hourData.currentTime
+                        ).toLocaleDateString("en-us", {
+                           hour: "numeric"
+                        })
+                     }}
+                  </p>
+                  <img 
+                     :src="``" 
+                     alt="" 
+                     class="w-auto h-[150px] object-cover"
+                  >
+                  <p class="text-xl">
+                     {{Math.round(hourData.temp)}}&deg;
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+      <hr class="border-white border-opacity-10 w-full"/>
+      <div class="max-w-screen-md w-full py-12"></div>
    </div>
 </template>
 
@@ -22,7 +102,7 @@ const getWeatherData = async ()=>{
          hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
       })
 
-      return weatherData
+      return weatherData.data
    }catch(err){
       console.error(err)
    }
